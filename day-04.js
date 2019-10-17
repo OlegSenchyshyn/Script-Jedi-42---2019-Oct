8595191e296f03e5b7ef260a9718f4f1b2a0ced7
OlegSenchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2019-10-17 10:45:30 +0300

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -1,14 +1,14 @@
 // https://www.codewars.com/kata/training-js-number-9-loop-statement-while-and-do-dot-while/train/javascript
 function padIt(str, n) {
-    while (n > 0) {
-        if (n % 2 === 0) {
-          str = str + '*';
-        } else {
-          str = '*' + str;
-        }
-          n--;
+  while (n > 0) {
+    if (n % 2 === 0) {
+      str = str + '*';
+    } else {
+      str = '*' + str;
     }
-    return str;
+    n--;
+  }
+  return str;
 }
 
 
@@ -16,42 +16,47 @@ function padIt(str, n) {
 function pickIt(arr) {
   const odd = [], even = [];
     for (let i = 0; i < arr.length; i++) {
-      if (arr[i] % 2 === 0) even.push(arr[i]);
-        else {
-          odd.push(arr[i]);
-        }
+      if (arr[i] % 2 === 0) {
+        even.push(arr[i]);
+      } else {
+        odd.push(arr[i]);
+      }
     }
-    return [odd, even]
+  return [odd, even];
 }
 
 
 // https://www.codewars.com/kata/training-js-number-11-loop-statement-break-continue/train/javascript
 function grabDoll(dolls) {
-	var bag = [];
-	for (let i = 0; i < dolls.length; i++) {
-		if (dolls[i] === 'Hello Kitty' || dolls[i] === 'Barbie doll') {
-			bag.push(dolls[i])
-		} else continue;
-
-		if (bag.length >= 3) break;
-	}
-	return bag;
+  const bag = [];
+  for (let i = 0; i < dolls.length; i++) {
+    if (dolls[i] === 'Hello Kitty' || dolls[i] === 'Barbie doll') {
+      bag.push(dolls[i]);
+    } else {
+      continue;
+    }
+    
+    if (bag.length >= 3) {
+      break;
+    }
+  }
+  return bag;
 }
 
 
 // https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
 function giveMeFive(obj) {
-	let arr = [];
-	for (let key in obj) {
-		let val = obj[key];
-		if (key.length === 5) {
-			arr.push(key);
-		}
-		if (val.length === 5) {
-			arr.push(val);
-		}
-	}
-	return arr;
+  let arr = [];
+  for (let key in obj) {
+    let val = obj[key];
+    if (key.length === 5) {
+      arr.push(key);
+    }
+    if (val.length === 5) {
+    arr.push(val);
+    }
+  }
+  return arr;
 }
 
 
