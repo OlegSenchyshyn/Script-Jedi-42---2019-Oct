7385ca6a7fdc9664a0c5da3b60eec7f459828e25
OlegSenchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2019-10-17 12:25:42 +0300

    Update day-03.js

--- day-03.js
+++ day-03.js
@@ -1,26 +1,22 @@
 // https://www.codewars.com/kata/training-js-number-6-basic-data-types-boolean-and-conditional-statements-if-dot-else/train/javascript
 function trueOrFalse(val) {
   if (val) {
-  return 'true';
-  }
-  else {
-  return 'false';
+    return 'true';
+  } else {
+    return 'false';
   }
 }
-
 
 // https://www.codewars.com/kata/training-js-number-7-if-dot-else-and-ternary-operator/train/javascript
 function saleHotdogs(n) {
-    if (n < 5) {
+  if (n < 5) {
     return n * 100;
-    }
-    else if (n >= 5 && n < 10) {
+  } else if (n >= 5 && n < 10) {
     return n * 95;
-    }
-    else {return n * 90;
-    }    
+  } else {
+    return n * 90;
+  }
 }
-
 
 // https://www.codewars.com/kata/training-js-number-8-conditional-statement-switch/train/javascript
 function howManydays(month) {
@@ -43,6 +39,6 @@ function howManydays(month) {
       break;
     case 2:
       days = 28;
-    }
-    return days;
+  }
+  return days;
 }
