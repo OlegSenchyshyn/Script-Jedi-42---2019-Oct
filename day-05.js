a14dd33e2f887fc55be1373cf3e46a322a77bad1
OlegSenchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2019-10-18 13:07:51 +0300

    day-05.js

--- day-05.js
+++ day-05.js
@@ -5,8 +5,3 @@ function correctTail(body, tail) {
 }
  
 
-//https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
-
-function hero(bullets, dragons) {
-  return bullets >= dragons * 2;
-}
