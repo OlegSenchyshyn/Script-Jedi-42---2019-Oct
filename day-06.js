daf4c9eff06f21dfcf9fcd86da20b7254f9f689b
OlegSenchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2019-10-18 13:08:47 +0300

    day-06.js

--- day-06.js
+++ day-06.js
@@ -4,3 +4,9 @@ function maxMultiple(divisor, bound) {
   return bound - (bound % divisor);
 }
 
+
+//https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
+
+function hero(bullets, dragons) {
+  return bullets >= dragons * 2;
+}
