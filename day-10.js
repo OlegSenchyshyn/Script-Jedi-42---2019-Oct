15152d172645ac0ad2e2a295e8f86952fd283c59
OlegSenchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2019-10-24 00:32:14 +0300

    Update day-10.js

--- day-10.js
+++ day-10.js
@@ -13,34 +13,26 @@ function alienLanguage(str){
 
 //https://www.codewars.com/kata/training-js-number-20-methods-of-string-object-charat-charcodeat-and-fromcharcode/train/javascript
 
-//Це на жаль не мій розв'язок, але я змінив його під себе
+//Це на жаль не мій розв'язок, але код я змінив його під себе
 function topSecret(str) {
-  let chars = str.split('');
+  const chars = str.split('');
   for (let i = 0; i < chars.length; i++)
-    if (chars[i] === ' ' || chars[i] === '!') {
-    } else if (chars[i] == 'A') {
-      chars[i] = 'X';
-    } else if (chars[i] == 'B') {
-      chars[i] = 'Y';
-    } else if (chars[i] == 'C') {
-      chars[i] = 'Z';
-    } else if (chars[i] == 'a') {
-      chars[i] = 'x';
-    } else if (chars[i] == 'b') {
-      chars[i] = 'y';
-    } else if (chars[i] == 'c') {
-      chars[i] = 'z';
-    } else {
-      chars[i] = String.fromCharCode(chars[i].charCodeAt() - 3);
-    }
+    if (chars[i] == ' ' || chars[i] == '!') {}
+    else if (chars[i] == 'A') chars[i] = 'X';
+    else if (chars[i] == 'B') chars[i] = 'Y';
+    else if (chars[i] == 'C') chars[i] = 'Z';
+    else if (chars[i] == 'a') chars[i] = 'x';
+    else if (chars[i] == 'b') chars[i] = 'y';
+    else if (chars[i] == 'c') chars[i] = 'z';
+    else chars[i] = String.fromCharCode(chars[i].charCodeAt() -3);
   return chars.join('');
 }
 //question1: The top secret file number is...
-answer1 = '2181';
+answer1="3745";
 //question2: Super agent's name is...
-answer2 = 'aFWB';
+answer2="RILc";
 //question3: He stole the treasure is...
-answer3 = 'Barbie doll';
+answer3="Expired biscuits";
 
 
 //https://www.codewars.com/kata/training-js-number-21-methods-of-string-object-trim-and-the-string-template/train/javascript
