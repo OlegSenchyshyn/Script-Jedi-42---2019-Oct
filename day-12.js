0052750cefcde6fcfa588fe5f962daa0ad6947aa
OlegSenchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2019-10-24 22:58:37 +0300

    Done

--- day-12.js
+++ day-12.js
@@ -42,7 +42,7 @@ ________________________________________________________________________________
 // To do: make all tests pass, leave the assert lines unchanged!
 // Follow the hints of the failure messages!
 
-describe('`add()` appends a new element to the end of a Set object.', function(){
+describe('`add()` appends a new element to the end of a Set object.', function() {
   let set;
   beforeEach(() => set = new Set());
   it('adds every value, of any type, only once', function() {
@@ -74,13 +74,16 @@ ________________________________________________________________________________
 // To do: make all tests pass, leave the assert lines unchanged!
 // Follow the hints of the failure messages!
 
-describe('`set.delete()` deletes an element from a set', function(){
+describe('`set.delete()` deletes an element from a set', function() {
   let set;
-  beforeEach(() => set = new Set());
-  
+  beforeEach(() => (set = new Set()));
+
   describe('use `delete(<value>)` to delete an element', function() {
     beforeEach(function() {
-      set.add('one').add('two').add('three');
+      set
+        .add('one')
+        .add('two')
+        .add('three');
     });
     it('`delete()` returns `true` when the element was found', function() {
       const returns = set.delete('one');
@@ -91,7 +94,7 @@ describe('`set.delete()` deletes an element from a set', function(){
       assert.equal(set.size, 2);
     });
   });
-  
+
   describe('if nothing was deleted (no element with the given value was found)', function() {
     it('returns `false`', function() {
       set.add('one');
@@ -99,14 +102,14 @@ describe('`set.delete()` deletes an element from a set', function(){
       assert.equal(returns, false);
     });
   });
-  
+
   describe('`undefined` is a valid value in a set', function() {
     it('deleting it, when it is not in the set, returns `false` too', function() {
-      let whatToDelete
+      let whatToDelete;
       assert.equal(set.delete(whatToDelete), false);
     });
     it('`delete()` removes it, when its in the set', function() {
-      set.add()
+      set.add();
       assert.equal(set.delete(), true);
     });
   });
@@ -115,5 +118,116 @@ describe('`set.delete()` deletes an element from a set', function(){
       set.add(1);
       assert.equal(set.delete('1'), false);
     });
+  });
+});
+
+_______________________________________________________________________________________________________
+// Set API http://tddbin.com/#?kata=es6/language/set/api
+// 65: Set - API overview
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('`Set` API overview', function() {
+  const api = [
+    'size',
+    'add',
+    'clear',
+    'delete',
+    'entries',
+    'forEach',
+    'has',
+    'keys',
+    'values'
+  ];
+  let set;
+  beforeEach(function() {
+    set = new Set(api);
+  });
+  it('a Set can be created from an array', function() {
+    let set = new Set(api);
+    assert.deepEqual(Array.from(set), api);
+  });
+  it('`size` is the number of values', function() {
+    const theSize = set.size;
+    assert.equal(theSize, api.length);
+  });
+  it('`add()` appends the given value', function() {
+    // Hint: To make the content of `api` and `set` consistent you can add the
+    // `Symbol.iterator` to the `set`. Strongly speaking it is missing in the API.
+    set.add(Symbol.iterator);
+    assert.equal(set.size, api.length + 1);
+  });
+  it('`clear()` removes all elements', function() {
+    set.clear();
+    assert.equal(set.size, 0);
+  });
+  it('`delete()` removes the given value', function() {
+    set.delete(`size`);
+    assert.equal(set.size, api.length - 1);
+  });
+  it('`entries()` returns an iterator for all values', function() {
+    const expectedEntries = api.map(entry => [entry, entry]);
+    const actualEntries = set.entries();
+    assert.deepEqual([...actualEntries], expectedEntries);
+  });
+  it('`forEach()` calls a callback for each value', function() {
+    let values = [];
+    set.forEach(value => {
+      values.push(value);
+    });
+    assert.deepEqual(values, api);
+  });
+  it('`has()` returns true if the given value is in the set', function() {
+    const propertyName = '';
+    set.add(propertyName);
+    assert.equal(set.has(propertyName), true);
+  });
+  describe('returns an iterator that contains all values', function() {
+    // In order to be alike to `Map`, `keys()` and `values()` are essentially the same thing for a `Set`.
+    it('`keys()`', function() {
+      const allKeys = set.keys();
+
+      assert.deepEqual([...allKeys], api);
+    });
+    it('`values()`', function() {
+      const allValues = set.keys();
+      assert.deepEqual([...allValues], api);
+    });
+    it('`[Symbol.iterator]()`', function() {
+      const iteratorKey = Symbol.iterator;
+      assert.deepEqual([...set[iteratorKey]()], api);
+    });
+  });
+});
+
+_______________________________________________________________________________________________________
+// set.clear() http://tddbin.com/#?kata=es6/language/set/clear
+// 70: Set - clear
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('`clear()` removes all elements from a Set object.', function() {
+  let set;
+  beforeEach(() => (set = new Set()));
+  it('`set.size` becomes 0', function() {
+    set.add('one').add(2);
+    set.clear();
+    let expectedSize = 0;
+    assert.equal(set.size, expectedSize);
+  });
+  it('the iterator `set.entries()` will not contain any items', function() {
+    set.add('one').add(2);
+    set.clear('done');
+    const { done } = set.entries().next();
+    assert.equal(done, true);
+  });
+  it('any call to `set.has()` returns false', function() {
+    set.add('one').add(2);
+    set.clear();
+    assert.deepEqual(set.has(2), false);
+  });
+  it('returns `undefined`', function() {
+    var expectedReturn;
+    assert.equal(set.clear(), expectedReturn);
   });
 });
