52d637c0c2f15272643732d1933ba68900390a6b
OlegSenchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2019-10-28 17:41:34 +0200

    Create day-15.js

---
+++ day-15.js
@@ -0,0 +1,18 @@
+//https://www.codewars.com/kata/training-js-number-22-unlock-new-skills-arrow-function-spread-operator-and-deconstruction/train/javascript
+
+const shuffleIt = (arr, ...shuffle) => {
+for (i of shuffle) {
+    let x = arr[i[0]];
+    arr[i[0]] = arr[i[1]];
+    arr[i[1]] = x;
+  }
+  return arr;
+}
+
+______________________________________________________________________________________________________________________________
+//http://www.codewars.com/kata/572af273a3af3836660014a1
+
+
+
+______________________________________________________________________________________________________________________________
+http://www.codewars.com/kata/572cb264362806af46000793
