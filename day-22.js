e1f8c7db1d90b6c65b5f78d8ed237de7ddd7de44
OlegSenchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2019-11-04 15:23:04 +0200

    Update day-22.js

--- day-22.js
+++ day-22.js
@@ -46,20 +46,20 @@ class LexicallyBound {
 
 describe('Arrow functions have lexical `this`, no dynamic `this`', () => {
   it('bound at definition time, use `=>`', function() {
-    var bound = new LexicallyBound();
-    var fn = bound.getFunction();
+    const bound = new LexicallyBound();
+    const fn = bound.getFunction();
     assert.strictEqual(fn(), bound);
   });
   it('can NOT bind a different context', function() {
-    var bound = new LexicallyBound();
-    var fn = bound.getFunction();
-    var anotherObj = {};
-    var expected = bound;
+    const bound = new LexicallyBound();
+    const fn = bound.getFunction();
+    const anotherObj = {};
+    const expected = bound;
     assert.strictEqual(fn.call(anotherObj), expected);
   });
   it('`arguments` does NOT work inside arrow functions', function() {
-    var bound = new LexicallyBound();
-    var fn = bound.getArgumentsFunction();
+    const bound = new LexicallyBound();
+    const fn = bound.getArgumentsFunction();
     assert.equal(fn(1, 2).length, 0);
   });
 });
