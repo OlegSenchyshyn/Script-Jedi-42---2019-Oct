849ced58670acc44db49a270ea5246a4601aad4f
OlegSenchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2019-11-06 17:43:22 +0200

    Create day-24.js

---
+++ day-24.js
@@ -0,0 +1,10 @@
+// Duplicate Arguments
+// https://www.codewars.com/kata/duplicate-arguments/train/javascript
+
+const solution = (...arr) => [...new Set(arr)].length !== arr.length; 
+
+//__________________________________________________________________________________
+//Last Argument
+// http://www.codewars.com/kata/last
+
+
