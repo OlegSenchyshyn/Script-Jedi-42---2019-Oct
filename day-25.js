66aa5e766d789f39bc5cee2c2a58bb59e241e276
OlegSenchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2019-11-07 16:24:27 +0200

    Create day-25.js

---
+++ day-25.js
@@ -0,0 +1,13 @@
+// Day 25 - Calling functions
+
+// Call function using apply
+// http://www.codewars.com/kata/write-javascripts-call-function-using-apply
+
+
+Function.prototype.call = function(val, ...args) {
+  return this.apply(val, args);
+};
+
+//__________________________________________________________________________
+// Anonymous returns 
+// http://www.codewars.com/kata/anonymous-returns
