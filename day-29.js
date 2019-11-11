c4658f2eda8ae5d9d68e304fa7924ac624e80cf6
OlegSenchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2019-11-11 22:15:31 +0200

    Create day-29.js

---
+++ day-29.js
@@ -0,0 +1,10 @@
+// Day 29 - Understanding new
+// How new works 
+// https://www.codewars.com/kata/how-new-works
+
+const myObj = Object.create(MyObject.prototype);
+              myObj.constructor();
+              
+//_________________________________________________________              
+// Replicate new
+// http://www.codewars.com/kata/replicate-new
